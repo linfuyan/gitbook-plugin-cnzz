2533ec3364b409aace0edae2aef11e4df669a697
Johan Preynat <johan.preynat@gmail.com>
2016-04-28 12:34:01 +0200

    Include script using GitBook 3 templating

--- index.js
+++ index.js
@@ -3,24 +3,6 @@ module.exports = {
         assets: "./book",
         js: [
             "plugin.js"
-        ],
-        html: {
-            "body:end": function() {
-                var visible = this.options.pluginsConfig.cnzz.visible;
-                var style = this.options.pluginsConfig.cnzz.style;
-                var siteid = this.options.pluginsConfig.cnzz.siteid;
-
-                console.log("visible:" + visible + ", style:" + style + ", siteid:" + siteid);
-
-                var stat = "<script src='http://s95.cnzz.com/z_stat.php?id="
-                    + siteid + "&web_id=" + siteid + "' language='JavaScript'></script>";
-
-                if (visible) {
-                    return stat;
-                } else {
-                    return "<div style='display:none'>" + stat + "</div>";
-                }
-            }
-        }
+        ]
     }
 };
